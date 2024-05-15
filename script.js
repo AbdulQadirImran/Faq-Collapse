be2199d0a01e4b2f3df0d51cbbe57fd9b37d9fd7
Abdul Qadir Imran <abdulqadirimran01@gmail.com>
2024-05-15 15:12:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const toggles = document.querySelectorAll('.faq-toggle')
-
 toggles.forEach(toggle => {
     toggle.addEventListener('click', () => {
         toggle.parentNode.classList.toggle('active')
